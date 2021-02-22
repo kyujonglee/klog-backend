4dcf19491afeb455b24bc004c56d162f87fe3ab8
이규종 <kyujong93@naver.com>
2021-02-22 23:12:43 +0900

    change env

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "db_klog"),
-        username: env("DATABASE_USERNAME", "kyujong"),
-        password: env("DATABASE_PASSWORD", "toddlf20"),
+        host: env("HOST", "localhost"),
+        port: env.int("PORT", 5432),
+        database: env("NAME", "db_klog"),
+        username: env("USERNAME", "kyujong"),
+        password: env("PASSWORD", "toddlf20"),
       },
       options: {
         autoMigration: true,
